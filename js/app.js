cacab12eab67587aaa7e6069f91796b8abf46c98
superpezo <l.zope13@gmail.com>
2026-06-19 13:15:18 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+const cursorLight = document.querySelector(".cursor-light");
 
+if (cursorLight) {
+  let mouseX = window.innerWidth / 2;
+  let mouseY = window.innerHeight / 2;
+  let glowX = mouseX;
+  let glowY = mouseY;
+
+  window.addEventListener("mousemove", event => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+  });
+
+  function animateGlow() {
+    glowX += (mouseX - glowX) * 0.08;
+    glowY += (mouseY - glowY) * 0.08;
+
+    cursorLight.style.left = `${glowX - 210}px`;
+    cursorLight.style.top = `${glowY - 210}px`;
+
+    requestAnimationFrame(animateGlow);
+  }
+
+  animateGlow();
+}
